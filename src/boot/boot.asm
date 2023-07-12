e3db6c52ccb45b4c81f0180b57509cb8466da53e
chemistr33 <chemistr33@proton.me>
2023-07-11 23:45:33 -0700

    Added a bunch of comments to better understand the code.

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -9,6 +9,7 @@ CODE_SEG equ gdt_code - gdt_start
 DATA_SEG equ gdt_data - gdt_start
 
 ;;;;;;;;;;;;;;;;;; BIOS PARAMETER BLOCK COMPATIBILITY LAYER ;;;;;;;;;;;;;;;;;;;;
+
 _start:
     jmp short start
     nop
@@ -17,7 +18,9 @@ times 33 db 0
 
 start:
 jmp 0:start2
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;  16-bit Real Mode ...  ;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 start2:
     ; Setup segments
     cli             ; disable interrupts
@@ -77,11 +80,12 @@ load32:
     in al, 0x92             ; read from IO bus port 0x92
     or al, 2                ; or in 2 to bit1 of al
     out 0x92, al            ; write al to IO bus port 0x92
-    ; 
+
+    ; Prepare registers for disk read routine 
     mov eax, 1              ; sets LBA to 1, the second sector 
     mov ecx, 100            ; sets sector count to 100
     mov edi, 0x0100000      ; sets destination address to 1MB, code segment
-    call ata_lba_read       ; calls the function ata_lba_read to load the kernel
+    call ata_lba_read       ; calls the function ata_lba_read to load kernel
                             ; into memory at 0x0100000 ... 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -91,55 +95,101 @@ load32:
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ata_lba_read:
-    mov ebx, eax        ; save LBA in ebx, eax is needed by IO port instructions            
-    shr eax, 24         ; shift eax right 24 bits...results in zero?
-    or eax, 0xE0        ; or in 0xE0 to bits 7-5 of eax
-    mov dx, 0x1F6       ; set dx to 0x1F6, the IO bus port for the drive/head,
-                        ; bit7 always 1, bit6=1 slave, bit5=1 LBA mode
-    out dx, al          ; write al to IO bus port 0x1F6
+    ; Save LBA in ebx, eax is needed by subsequent IO port instructions
+    mov ebx, eax                    
 
+    ; Prepare the drive/head register
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; 
+    shr eax, 24     ; Right-Shift EAC by 24, leaving MS Byte (value 0)
+    or eax, 0xE0    ; OR in 0xE0 to set bits 7:5 of eax 
+                    ; -> bit7 always 1, bit6=1 slave, bit5=1 LBA mode
+    mov dx, 0x1F6   ; Set dx to 0x1F6, the IO bus port for the drive/head,
+    out dx, al      ; Write al OUT to IO bus port 0x1F6
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    mov eax, ecx
-    mov dx, 0x1F2
-    out dx, al
+    ; Prepare the sector count register
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    mov eax, ecx    ; Prepare eax with the sector count 
+    mov dx, 0x1F2   ; Prepare dx to 0x1F2, the IO bus port for sector count
+    out dx, al      ; Write al(sector count) OUT to IO bus port 0x1F2
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    mov eax, ebx 
-    mov dx, 0x1F3
-    out dx, al
+    ; Prepare the LBA low register
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    mov eax, ebx    ; Restore ebx to eax
+    mov dx, 0x1F3   ; Prepare dx to 0x1F3, the IO bus port for LBA low byte
+    out dx, al      ; Write al(LBA low byte) OUT to IO bus port 0x1F3
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    mov dx, 0x1F4
-    mov eax, ebx 
-    shr eax, 8
-    out dx, al
+    ; Prepare the LBA mid register
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    mov eax, ebx    ; Restore ebx to eax
+    mov dx, 0x1F4   ; Prepare dx to 0x1F4, the IO bus port for LBA mid byte
+    shr eax, 8      ; Right-Shift eax by 8, leaving mid byte
+    out dx, al      ; Write al(LBA mid byte) OUT to IO bus port 0x1F4
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    mov dx, 0x1F5
-    mov eax, ebx
-    shr eax, 16
-    out dx, al
+    ; Prepare the LBA high register
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    mov eax, ebx    ; Restore ebx to eax
+    mov dx, 0x1F5   ; Prepare dx to 0x1F5, the IO bus port for LBA high byte
+    shr eax, 16     ; Right-Shift eax by 16, leaving high byte
+    out dx, al      ; Write al(LBA high byte) OUT to IO bus port 0x1F5
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-    mov dx, 0x1F7
-    mov al, 0x20
-    out dx, al
-
+    ; Prepare the command register
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    mov dx, 0x1F7   ; Prepare dx to 0x1F7, the IO bus port for command                      
+    mov al, 0x20    ; Prepare al to 0x20, the command for read
+    out dx, al      ; Write al(command) OUT to IO bus port 0x1F7
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    ; Read the data from the disk.
+    ; Read 256 words (512 bytes) at a time.
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
     .next_sector:
-        push ecx
+        push ecx            ; Save ecx, count of words to read aka loop variable
 
     .try_again:
-        mov dx, 0x1F7
-        in al, dx
-        test al, 8
-        jz .try_again
+        mov dx, 0x1F7       ; Prepare dx to 0x1F7, the IO bus port for status
+        in al, dx           ; Read from IO bus port 0x1F7 into al
+        test al, 8          ; Test bit 3 of al, if 1, disk is ready
+        jz .try_again       ; If not ready, try again
 
-        mov ecx, 256
-        mov dx, 0x1F0
-        rep insw
-        pop ecx
-        loop .next_sector
+        mov ecx, 256        ; Prepare ecx with 256, the number of words to read
+        mov dx, 0x1F0       ; Prepare dx to 0x1F0, the IO bus port for data
+        rep insw            ; Read 256 words from IO bus port 0x1F0 into es:edi
+        pop ecx             ; Restore ecx 
+        loop .next_sector   ; Decrement ecx, if ecx != 0, jump to .next_sector
         ret
+
+;          *28-bit LBA Address Format*
+; +--------------------+----------------------+
+; |   LBA Address (0-23) |   Reserved (24-27) |
+; |                      |                    |
+; +-------------------+-----------------------+
+; |        ATA LBA Address (32 bits)          |
+; +-------------------+-----------------------+
+;
+;   1F0 (Read and Write): Data Register
+;   1F1 (Read): Error Register
+;   1F1 (Write): Features Register
+;   1F2 (Read and Write): Sector Count Register
+;   1F3 (Read and Write): LBA Low Register
+;   1F4 (Read and Write): LBA Mid Register
+;   1F5 (Read and Write): LBA High Register
+;   1F6 (Read and Write): Drive/Head Register
+;   1F7 (Read): Status Register
+;   1F7 (Write): Command Register
+;   3F6 (Read): Alternate Status Register
+;   3F6 (Write): Device Control Register
+;
+;   Writing to Command Register (1F7) initiates a command
+;   -> 0x20 = Read Sectors with Retry
+;   -> 0x30 = Write Sectors with Retry
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;; Pad the boot sector out to 510 bytes and add 2B boot signature  ;;;;;;;;;
 times 510-($-$$) db 0
 dw 0xAA55
-;;;;;;.................................................................;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
