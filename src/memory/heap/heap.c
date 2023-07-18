813276ee8630935f3b1c3401151dfc4988a79076
chemistr33 <chemistr33@proton.me>
2023-07-17 20:31:44 -0700

    Fixed bug in heap.c, bs = i not bs = 1 ...

--- src/memory/heap/heap.c
+++ src/memory/heap/heap.c
@@ -1,199 +1,190 @@
 #include "heap.h"
 #include "kernel.h"
-#include "status.h"
 #include "memory/memory.h"
+#include "status.h"
 #include <stdbool.h>
 
-static int 
-heap_check_table(void *ptr,
-                            void *end,
-                            struct heap_table* table)
+static int
+heap_check_table (void *ptr, void *end, struct heap_table *table)
 {
   int res = 0;
   size_t table_size = (size_t)(end - ptr);
   size_t total_blocks = table_size / LAMEOS_HEAP_BLOCK_SIZE;
-  if(table->total != total_blocks)
-  {
-    res = -EINVARG;
-    goto out;
-  }
-  out:
-    return res;  
+  if (table->total != total_blocks)
+    {
+      res = -EINVARG;
+      goto out;
+    }
+out:
+  return res;
 }
 
-static 
-bool heap_check_alignment(void *ptr){
+static bool
+heap_check_alignment (void *ptr)
+{
   return ((unsigned int)ptr % LAMEOS_HEAP_BLOCK_SIZE) == 0;
 }
 
 int
-heap_create (struct heap *heap,
-             void *ptr,
-             void *end,
-             struct heap_table *table)
+heap_create (struct heap *heap, void *ptr, void *end, struct heap_table *table)
 {
   int res = 0;
-  if(!heap_check_alignment(ptr) || !heap_check_alignment(end))
-  {
-    res = -EINVARG;
-    goto out;
-  }
-  memset(heap, 0, sizeof(struct heap));
+  if (!heap_check_alignment (ptr) || !heap_check_alignment (end))
+    {
+      res = -EINVARG;
+      goto out;
+    }
+
+  memset (heap, 0, sizeof (struct heap));
   heap->saddr = ptr;
   heap->table = table;
 
-  res = heap_check_table(ptr, end, table);
-  if(res<0) goto out;
+  res = heap_check_table (ptr, end, table);
+  if (res < 0)
+    {
+      goto out;
+    }
 
-  size_t table_size = sizeof(HEAP_BLOCK_TABLE_ENTRY) * table->total;
-  memset(table->entries, HEAP_BLOCK_TABLE_ENTRY_FREE, table_size);
+  size_t table_size = sizeof (HEAP_BLOCK_TABLE_ENTRY) * table->total;
+  memset (table->entries, HEAP_BLOCK_TABLE_ENTRY_FREE, table_size);
 
-  out:
-    return res;
+out:
+  return res;
 }
 
-static uint32_t heap_align_value_to_upper(uint32_t val)
+static uint32_t
+heap_align_value_to_upper (uint32_t val)
 {
-  if((val % LAMEOS_HEAP_BLOCK_SIZE) == 0)
-  {
-    return val;
-  }
+  if ((val % LAMEOS_HEAP_BLOCK_SIZE) == 0)
+    {
+      return val;
+    }
 
   val = (val - (val % LAMEOS_HEAP_BLOCK_SIZE));
   val += LAMEOS_HEAP_BLOCK_SIZE;
   return val;
 }
 
-static int heap_get_entry_type(HEAP_BLOCK_TABLE_ENTRY entry)
+static int
+heap_get_entry_type (HEAP_BLOCK_TABLE_ENTRY entry)
 {
   return entry & 0x0f;
 }
 
-int heap_get_start_block(struct heap *heap, uint32_t total_blocks)
+int
+heap_get_start_block (struct heap *heap, uint32_t total_blocks)
 {
-  struct heap_table* table = heap->table;
+  struct heap_table *table = heap->table;
   int bc = 0;
   int bs = -1;
 
-  for(size_t i=0; i<table->total; i++)
-  {
-    if(heap_get_entry_type(table->entries[i]) != HEAP_BLOCK_TABLE_ENTRY_FREE)
+  for (size_t i = 0; i < table->total; i++)
     {
-      bc = 0;
-      bs = -1;
-      continue;
+      if (heap_get_entry_type (table->entries[i])
+          != HEAP_BLOCK_TABLE_ENTRY_FREE)
+        {
+          bc = 0;
+          bs = -1;
+          continue;
+        }
+      if (bs == -1)
+        {
+          bs = i;
+        }
+      bc++;
+      if (bc == total_blocks)
+        {
+          break;
+        }
     }
-
-    if(bs == -1)
-    {
-      bs = 1;
-    }
-    bc++;
-    if(bc == total_blocks)
-    {
-      break;
-    }
-  }
-  
   if (bs == -1)
-  {
-    return -ENOMEM;
-  }
+    {
+      return -ENOMEM;
+    }
   return bs;
 }
 
-void * heap_block_to_address(struct heap *heap, uint32_t block)
+void *
+heap_block_to_address (struct heap *heap, uint32_t block)
 {
   return heap->saddr + (block * LAMEOS_HEAP_BLOCK_SIZE);
 }
 
-void 
-heap_mark_blocks_taken(struct heap *heap, int start_block, int total_blocks)
+void
+heap_mark_blocks_taken (struct heap *heap, int start_block, int total_blocks)
 {
   int end_block = (start_block + total_blocks) - 1;
   // assert block is in bounds?
 
-  HEAP_BLOCK_TABLE_ENTRY entry = HEAP_BLOCK_TABLE_ENTRY_TAKEN | HEAP_BLOCK_IS_FIRST;
-  if(total_blocks > 1)
-  {
-    entry |= HEAP_BLOCK_HAS_NEXT;
-  }
-
-  for(int i = start_block; i<=end_block; i++)
-  {
-    heap->table->entries[i] = entry;
-    entry = HEAP_BLOCK_TABLE_ENTRY_TAKEN;
-    if (i != end_block - 1)
+  HEAP_BLOCK_TABLE_ENTRY entry
+      = HEAP_BLOCK_TABLE_ENTRY_TAKEN | HEAP_BLOCK_IS_FIRST;
+  if (total_blocks > 1)
     {
       entry |= HEAP_BLOCK_HAS_NEXT;
     }
-  }
+
+  for (int i = start_block; i <= end_block; i++)
+    {
+      heap->table->entries[i] = entry;
+      entry = HEAP_BLOCK_TABLE_ENTRY_TAKEN;
+      if (i != end_block - 1)
+        {
+          entry |= HEAP_BLOCK_HAS_NEXT;
+        }
+    }
 }
 
-void * 
-heap_malloc_blocks(struct heap *heap, uint32_t total_blocks)
+void *
+heap_malloc_blocks (struct heap *heap, uint32_t total_blocks)
 {
   void *address = 0;
 
-  int start_block = heap_get_start_block(heap, total_blocks);
-  if(start_block < 0)
-  {
-    goto out;
-  }
+  int start_block = heap_get_start_block (heap, total_blocks);
+  if (start_block < 0)
+    {
+      goto out;
+    }
 
-  address = heap_block_to_address(heap, start_block);
+  address = heap_block_to_address (heap, start_block);
 
   // Mark the blocks as taken
-  heap_mark_blocks_taken(heap, start_block, total_blocks);
+  heap_mark_blocks_taken (heap, start_block, total_blocks);
 
 out:
   return address;
 }
 
-void heap_mark_blocks_free(struct heap *heap, int start_block)
+void
+heap_mark_blocks_free (struct heap *heap, int start_block)
 {
   struct heap_table *table = heap->table;
-  for(int i = start_block; i < (int)table->total; i++)
-  {
-    HEAP_BLOCK_TABLE_ENTRY entry = table->entries[i];
-    table->entries[i] = HEAP_BLOCK_TABLE_ENTRY_FREE;
-    if(!(entry & HEAP_BLOCK_HAS_NEXT))
+  for (int i = start_block; i < (int)table->total; i++)
     {
-      break;
+      HEAP_BLOCK_TABLE_ENTRY entry = table->entries[i];
+      table->entries[i] = HEAP_BLOCK_TABLE_ENTRY_FREE;
+      if (!(entry & HEAP_BLOCK_HAS_NEXT))
+        {
+          break;
+        }
     }
-  }
 }
 
-int heap_address_to_block(struct heap *heap, void *address)
+int
+heap_address_to_block (struct heap *heap, void *address)
 {
   return ((int)(address - heap->saddr)) / LAMEOS_HEAP_BLOCK_SIZE;
 }
 
-
-void * 
-heap_malloc(struct heap *heap, size_t size)
+void *
+heap_malloc (struct heap *heap, size_t size)
 {
-  size_t aligned_size = heap_align_value_to_upper(size);
+  size_t aligned_size = heap_align_value_to_upper (size);
   uint32_t total_blocks = aligned_size / LAMEOS_HEAP_BLOCK_SIZE;
-  return heap_malloc_blocks(heap, total_blocks);
+  return heap_malloc_blocks (heap, total_blocks);
 }
 
-void heap_free(struct heap *heap, void *ptr)
+void
+heap_free (struct heap *heap, void *ptr)
 {
-  heap_mark_blocks_free(heap, heap_address_to_block(heap, ptr));
+  heap_mark_blocks_free (heap, heap_address_to_block (heap, ptr));
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
