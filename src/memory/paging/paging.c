3501b3d01ee35e87c1a3a63ca586d5fe3b90947b
chemistr33 <chemistr33@proton.me>
2023-07-29 22:23:01 -0700

    Implemented basic task functionality, process concept upcoming.

--- src/memory/paging/paging.c
+++ src/memory/paging/paging.c
@@ -40,6 +40,20 @@ paging_switch (uint32_t *directory)
   current_directory = directory;
 }
 
+void
+paging_free_4gb (struct paging_4gb_chunk *chunk)
+{
+  for (int i = 0; i < 1024; i++)
+    {
+      uint32_t entry = chunk->directory_entry[i];
+      uint32_t *table = (uint32_t *)(entry & 0xFFFFF000);
+      kfree (table);
+    }
+
+  kfree (chunk->directory_entry);
+  kfree (chunk);
+}
+
 uint32_t *
 paging_4gb_chunk_get_directory (struct paging_4gb_chunk *chunk)
 {
@@ -75,23 +89,24 @@ out:
   return res;
 }
 
-int paging_set(uint32_t *directory, void *virt, uint32_t val)
+int
+paging_set (uint32_t *directory, void *virt, uint32_t val)
 {
-  if(!paging_is_aligned(virt))
-  {
-    return -EINVARG;
-  }
+  if (!paging_is_aligned (virt))
+    {
+      return -EINVARG;
+    }
 
   uint32_t directory_index = 0;
   uint32_t table_index = 0;
-  int res = paging_get_indices(virt, &directory_index, &table_index);
-  if(res<0)
-  {
-    return res;
-  }
+  int res = paging_get_indices (virt, &directory_index, &table_index);
+  if (res < 0)
+    {
+      return res;
+    }
 
   uint32_t entry = directory[directory_index];
-  uint32_t *table = (uint32_t*)(entry & 0xfffff000);
+  uint32_t *table = (uint32_t *)(entry & 0xfffff000);
   table[table_index] = val;
 
   return res;
